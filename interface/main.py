89ad8c74fb7747b53b591a6481f2efd69ae5330c
vschmidt <schmidtvinicio@gmail.com>
2021-05-23 13:57:01 -0300

    Adicionado a FFT

--- interface/main.py
+++ interface/main.py
@@ -6,9 +6,7 @@ import matplotlib.pyplot as plt
 CHUNK = 1024 * 2
 FORMAT = pa.paInt16
 CHANNELS = 1
-DEVICE = 6
 RATE = 44100  # Em Hz
-
 
 p = pa.PyAudio()
 
@@ -18,8 +16,7 @@ stream = p.open(
     rate=RATE,
     input=True,
     output=True,
-    frames_per_buffer=CHUNK,
-    input_device_index=DEVICE
+    frames_per_buffer=CHUNK
 )
 
 data = stream.read(CHUNK)
@@ -27,11 +24,30 @@ data = stream.read(CHUNK)
 # Converter os binários para inteiros
 dataInt = struct.unpack(str(CHUNK) + 'h', data)
 
-# Plotar o resultado
-# fig, ax = plt.subplots()
-# x = np.arange(0, 2*CHUNK, 2)
-# line, = ax.plot(x, np.random.rand(CHUNK), 'r')
+# Configurações da figura
+fig, (ax1, ax2) = plt.subplots(2)
 
-# fig.show()
+x = np.arange(0, 2*CHUNK, 2)
+x_fft = np.linspace(0, RATE, CHUNK)
 
+line, = ax1.plot(x, np.random.rand(CHUNK), 'r')
+line_fft, = ax2.semilogx(x_fft, np.random.rand(CHUNK))
 
+ax1.set_ylim(-60000, 60000)
+ax1.set_xlim(0, CHUNK)
+
+ax2.set_ylim(0, 0.4)
+ax2.set_xlim(20, RATE/2)
+
+fig.show()
+
+# Loop de leitura
+while True:
+    data = stream.read(CHUNK)
+    dataInt = struct.unpack(str(CHUNK) + 'h', data)
+    
+    line.set_ydata(dataInt)
+    line_fft.set_ydata(np.abs(np.fft.fft(dataInt))*2/(33000*CHUNK))
+
+    fig.canvas.draw()
+    fig.canvas.flush_events()
